b3967c44a328ebb65b0eb6e14b8a63021a67791a
Jaime Guerra <61339190+jguerra24@users.noreply.github.com>
2022-02-23 18:30:59 -0700

    Comienzo de la app y estilos

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <section class="todoapp">
+    <header>
+      <h1>Tareas</h1>
+      <input
+        type="text"
+        class="new-todo"
+        autofocus
+        autocomplete="off"
+        placeholder="Escribe tu tarea pendiente aquí..."
+        v-model="newTodo"
+        @keyup.enter="addTodo()"
+      />
+    </header>
+    <section class="main" v-show="todos.length">
+      <ul>
+        <li class="todo-list" v-for="todo in todos" :key="todo.id">
+          <input class="toggle" type="checkbox" />
+          {{ todo.title }} | {{ todo.completed }}
+        </li>
+      </ul>
+    </section>
+  </section>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    //todos nuestros datos
+    todos: [],
+    newTodo: "",
+  }),
+  methods: {
+    //todos los metodos
+    addTodo() {
+      const value = this.newTodo && this.newTodo.trim();
+
+      const todoItem = {
+        id: this.todos.length + 1,
+        title: value,
+        completed: false,
+      };
+
+      if (!value) {
+        return;
+      }
+
+      this.todos.push(todoItem);
+      this.newTodo = "";
+    },
+  },
+};
+</script>
+
 <style>
+@import "./styles/todomvc-base.css";
+@import "./styles/todomvc-index.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
